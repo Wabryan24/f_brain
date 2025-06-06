0482379d1935fb7b4a7738a04bf104c9b9ad2488
Wabryan24 <Sylvainqwerty39@gmail.com>
2025-06-06 17:25:36 +0200

    Ajout des statistiques aux professeurs

--- server/core/serializers.py
+++ server/core/serializers.py
@@ -1,14 +1,27 @@
+# core/serializers.py
+
 from rest_framework import serializers
 from .models import Professor, Vote
-
-
-class ProfessorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Professor
-        fields = '__all__'
-
 
 class VoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vote
         fields = '__all__'
+
+class ProfessorSerializer(serializers.ModelSerializer):
+    nombre_votes = serializers.SerializerMethodField()
+    moyenne_notes = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Professor
+        fields = ['id', 'name', 'description', 'nombre_votes', 'moyenne_notes']
+
+    def get_nombre_votes(self, obj):
+        return obj.votes.count()
+
+    def get_moyenne_notes(self, obj):
+        votes = obj.votes.all()
+        if votes.exists():
+            total = sum(v.score for v in votes)
+            return round(total / votes.count(), 2)
+        return None
