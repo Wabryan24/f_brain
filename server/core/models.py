0bb91a054fe27fec64ffd90d3c3f9815d31464c6
Wabryan24 <Sylvainqwerty39@gmail.com>
2025-06-06 16:42:00 +0200

    Création page votes et professors

--- server/core/models.py
+++ server/core/models.py
@@ -1,12 +1,21 @@
-# Create your models here.
 from django.db import models
+
+# Liste des choix de score de 1 à 5 étoiles
+SCORE_CHOICES = [(i, '⭐' * i) for i in range(1, 6)]
 
 class Professor(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
 
+    def __str__(self):
+        return self.name
+
 class Vote(models.Model):
-    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE, related_name='votes')
     ip_address = models.GenericIPAddressField()
     timestamp = models.DateTimeField(auto_now_add=True)
     comment = models.TextField(blank=True, null=True)
+    score = models.IntegerField(choices=SCORE_CHOICES)
+
+    def __str__(self):
+        return f"{self.professor.name} - {self.score}⭐"
--- server/core/views.py
+++ server/core/views.py
@@ -1,14 +1,29 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Count
 from .models import Professor, Vote
 from .serializers import ProfessorSerializer, VoteSerializer
+
 
 class ProfessorViewSet(viewsets.ModelViewSet):
     queryset = Professor.objects.all()
     serializer_class = ProfessorSerializer
 
+    @action(detail=False, methods=["get"], url_path="vote-results")
+    def vote_results(self, request):
+        """
+        Custom endpoint: /api/professors/vote-results/
+        Returns the number of votes per professor.
+        """
+        results = (
+            Vote.objects.values("professor__name")
+            .annotate(vote_count=Count("id"))
+            .order_by("-vote_count")
+        )
+        return Response(results)
+
+
 class VoteViewSet(viewsets.ModelViewSet):
     queryset = Vote.objects.all()
     serializer_class = VoteSerializer
-from django.shortcuts import render
-
-# Create your views here.
