dd991d264f8bddf90723053a1426535104d7b709
Wabryan24 <Sylvainqwerty39@gmail.com>
2025-06-07 09:25:07 +0200

    Mofification du port du frontend, creation du dashboard pour les votes, creation du formulaire de vote(en cours de réalisation)

--- client/src/hooks/api.js
+++ client/src/hooks/api.js
@@ -1,2 +1,11 @@
+// client/src/hooks/api.js
 import axios from "axios";
+
 export const api = axios.create({ baseURL: "http://web:8000/" });
+
+// Récupérer tous les professeurs avec nombre de votes et moyenne
+export const fetchProfessors = async () => {
+  const response = await api.get("api/professors/");
+  return response.data;
+};
+
